cce49abf0b5b5b3a9c0ddfe0b0982789f33d32cf
UserUNP <ahmedelazzouzi434@gmail.com>
2022-07-26 18:37:46 +0100

    📄 docs: added JSDoc to Vector.ts

--- src/values/Vector.ts
+++ src/values/Vector.ts
@@ -2,7 +2,10 @@ import Value from "../components/Value";
 
 export default class Vector extends Value {
 	/**
-	 * 
+	 * Create a new vector value.
+	 * @param x X coordinate.
+	 * @param y Y coordinate.
+	 * @param z Z coordinate.
 	 */
 	constructor(public x: number, public y: number, public z: number, slot?: number) {
 		super("vec", { x, y, z }, slot);
