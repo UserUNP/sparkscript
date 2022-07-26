5ac32d5961934b2c829099c969f1bcccbc83e918
UserUNP <ahmedelazzouzi434@gmail.com>
2022-07-26 18:56:01 +0100

    🐞 fix: apparently its in ticks not seconds

--- src/values/Potion.ts
+++ src/values/Potion.ts
@@ -4,7 +4,7 @@ export default class Potion extends Value {
 	/**
 	 * Create a new potion value.
 	 * @param potion The potion name.
-	 * @param duration The duration of the potion.
+	 * @param duration The duration of the potion in ticks.
 	 * @param amplifier Strength of the potion.
 	 */
 	constructor(public potion: string, public duration: number, public amplifier: number, slot?: number) {
