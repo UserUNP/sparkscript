babf04ab8c9fa37a9394618bc1751079095a5de6
UserUNP <39243708+UserUNP@users.noreply.github.com>
2022-09-25 11:55:47 +0000

    🚑 yeepee;

--- src/core/DFTarget.ts
+++ src/core/DFTarget.ts
@@ -1,3 +1,3 @@
-type DFTarget = "Selection"|"Default"|"AllPlayers"|"Victim"|"Killer"|"Damager"|"Shooter"|"Projectile"|"LastEntity"
+type DFTarget = "Selection"|"Default"|"AllPlayers"|"Victim"|"Killer"|"Damager"|"Shooter"|"Projectile"|"LastEntity";
 
 export default DFTarget;
