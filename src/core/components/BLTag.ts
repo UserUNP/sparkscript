2ff9101abeb67f609da93ea42605038970351fd4
UserUNP <39243708+UserUNP@users.noreply.github.com>
2022-11-17 05:51:19 +0100

    🩹 build failing due to BLTag being unimplemented

--- src/core/components/BLTag.ts
+++ src/core/components/BLTag.ts
@@ -1,7 +1,8 @@
 import DFBlockCodename from "../types/DFBlockCodename";
 import Value from "./Value";
 
-export interface Ibl_tag<T extends DFBlockCodename> {
+export interface Ibl_tag
+<T extends DFBlockCodename = DFBlockCodename> {
 	block: T;
 	action: string;
 	tag: string;
@@ -10,7 +11,12 @@ export interface Ibl_tag<T extends DFBlockCodename> {
 
 export default class BLTag<T extends DFBlockCodename = DFBlockCodename> extends Value<"bl_tag", Ibl_tag<T>> {
 
-	constructor(slotFromLastIndex: number = 0) {
-		super("bl_tag", {}, 26-slotFromLastIndex);
+	constructor(blkCodename: T, slotFromLastIndex: number = 0) {
+		super("bl_tag", {
+			block: blkCodename,
+			action: "(unimplemented)",
+			option: "(unimplemented)",
+			tag: "(unimplemented)"
+		}, 26-slotFromLastIndex);
 	}
 }
